1f6df70bba3a8c03381e6a0de9aaf0690f538081
dong.dam <dong.dam@tda.company>
2023-05-25 09:51:27 +0700

    feat: config firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:shop_laptop_project/firebase_options.dart';
 
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await configureApp();
   runApp(const App());
 }
