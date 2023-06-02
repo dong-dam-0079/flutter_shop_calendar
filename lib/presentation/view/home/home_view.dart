a9c4314302e55708e30abb468035f377039c338e
dong.dam <dong.dam@tda.company>
2023-06-02 17:36:12 +0700

    feat: add calendar

--- lib/presentation/view/home/home_view.dart
+++ lib/presentation/view/home/home_view.dart
@@ -1,16 +1,13 @@
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:calendar_date_picker2/calendar_date_picker2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:get/get.dart';
 import 'package:shop_laptop_project/config/di/app_module.dart';
 import 'package:shop_laptop_project/config/router/routers/home_router.dart';
 import 'package:shop_laptop_project/presentation/view/home/controller/home_controller.dart';
 import 'package:shop_laptop_project/presentation/widgets/common_app_bar.dart';
-import 'package:shop_laptop_project/presentation/widgets/common_list_shop.dart';
 import 'package:shop_laptop_project/presentation/widgets/common_text_styles.dart';
 
 import '../../../common/constants/assets.dart';
-import '../../../common/constants/common_constants.dart';
 import '../../../common/res/colors.dart';
 import '../../../common/res/dimens.dart';
 import '../../../generated/l10n.dart';
@@ -25,6 +22,7 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   final _homeController = serviceLocator<HomeController>();
+  List<DateTime?> initDate = [DateTime.now()];
 
   @override
   void initState() {
@@ -48,103 +46,20 @@ class _HomeViewState extends State<HomeView> {
           right: DimensRes.sp16,
           top: DimensRes.sp16,
         ),
-        child: Obx(
-          () => _homeController.isLoading.value
-              ? const Center(child: CircularProgressIndicator())
-              : Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      S.current.title_new,
-                      style: CommonTextStyles.largeBold,
-                    ),
-                    Gaps.vGap8,
-                    Obx(
-                      () => SizedBox(
-                        height: MediaQuery.of(context).size.height / 3.5,
-                        child: _buildListNew(),
-                      ),
-                    ),
-                    Gaps.vGap16,
-                    Text(
-                      S.current.title_popular,
-                      style: CommonTextStyles.largeBold,
-                    ),
-                    Gaps.vGap8,
-                    Expanded(
-                      child: CommonListShop(
-                        isVertical: true,
-                        shop: _homeController.shopPopular,
-                      ),
-                    ),
-                  ],
-                ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: CalendarDatePicker2(
+                config: CalendarDatePicker2Config(
+                    calendarType: CalendarDatePicker2Type.single),
+                value: initDate,
+                onValueChanged: (dates) {},
+              ),
+            ),
+          ],
         ),
       ),
-    );
-  }
-
-  Widget _buildListNew() {
-    return ListView.builder(
-      itemCount: _homeController.shopNews.length,
-      shrinkWrap: true,
-      scrollDirection: Axis.horizontal,
-      itemBuilder: (context, index) {
-        var news = _homeController.shopNews[index];
-
-        return InkWell(
-          onTap: () {
-            HomeRouter.goDetail(context, shopModel: news);
-          },
-          child: Container(
-            width: MediaQuery.of(context).size.width / 2.8,
-            margin: const EdgeInsets.only(right: DimensRes.sp12),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: Container(
-                    height: 120,
-                    margin: const EdgeInsets.all(DimensRes.sp4),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(DimensRes.sp16),
-                      color: ColorsRes.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: ColorsRes.primary.withOpacity(0.1),
-                          spreadRadius: 1,
-                          blurRadius: 3,
-                          offset: const Offset(0, 0.5),
-                        )
-                      ],
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(DimensRes.sp16),
-                      child: CachedNetworkImage(
-                        fit: BoxFit.fill,
-                        imageUrl: news.linkImage ?? CommonConstants.defaultLinkImg,
-                        errorWidget: (context, url, error) {
-                          return Image.asset(
-                            Assets.imgSetting,
-                            fit: BoxFit.fill,
-                          );
-                        },
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: CommonItemList(
-                    isVertical: false,
-                    item: news,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
     );
   }
 
@@ -199,16 +114,6 @@ class _HomeViewState extends State<HomeView> {
           S.current.button_go_favorite,
           Assets.icFavorite,
         ),
-        // const PopupMenuItem(
-        //   padding: EdgeInsets.zero,
-        //   height: DimensRes.sp2,
-        //   child: Gaps.vLine,
-        // ),
-        // _buildItemMenu(
-        //   value: 2,
-        //   S.current.button_go_history,
-        //   Assets.icHistory,
-        // ),
       ],
       onSelected: (value) {
         if (value == 1) {
